9e3d7bf9a48275641f4d9cfe508037868c79df73
Aliaksandr Razumovich <aliaksandr.razumovich@gmail.com>
2021-04-26 05:45:59 +0300

    Task-4: Adjust FE to support product creation

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -46,8 +46,8 @@ export default function Products() {
           <Card className={classes.card}>
             <CardMedia
               className={classes.cardMedia}
-              image={`https://source.unsplash.com/random?sig=${index}`}
-              title="Image title"
+              image={product.image_url || `https://source.unsplash.com/random?sig=${index}`}
+              title={product.title}
             />
             <CardContent className={classes.cardContent}>
               <Typography gutterBottom variant="h5" component="h2">
