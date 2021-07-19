4023ec559a79a85a588b38718b5f4c3561ab96f4
Aliaksandr Razumovich <aliaksandr.razumovich@gmail.com>
2021-07-19 06:01:51 +0300

    Task-7: Lambda Authorizer

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://7xhce88i5a.execute-api.us-east-1.amazonaws.com/dev',
+  import: 'https://p1ss0vtvhl.execute-api.eu-west-1.amazonaws.com/dev',
   bff: ' https://2vb3ipgtlk.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
