599165cd23f3634bb707cd0f9ae1d16a51e4f6fd
Aliaksandr Razumovich <aliaksandr.razumovich@gmail.com>
2021-07-19 17:36:05 +0300

    Task-8: ELB

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -37,7 +37,7 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
         params: {
           name: encodeURIComponent(file.name)
         },
-        headers: { Authorization: `Basic ${localStorage.getItem('authorizationToken')}` }
+        headers: { Authorization: `Basic ${localStorage.getItem('authorization_token')}` }
       })
       console.log('File to upload: ', file.name)
       console.log('Uploading to: ', response.data)
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://p1ss0vtvhl.execute-api.eu-west-1.amazonaws.com/dev',
   bff: ' https://2vb3ipgtlk.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://razumovich-cart-api-gsdev.us-east-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
